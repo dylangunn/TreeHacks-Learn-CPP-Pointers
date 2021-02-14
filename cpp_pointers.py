6a3d43bba10e491f1b7737615763de4f84d7f4e3
Dylan Gunn <dylankgunn@gmail.com>
2021-02-13 18:00:00 -0800

    Added base functionality

---
+++ cpp_pointers.py
@@ -0,0 +1,45 @@
+
+coverAll = 0
+
+
+def Intro():
+    print("Pointers are among the most consistently confusing topics for new programmers. Even so, they don’t have to be! Once you get some practice you’ll see just how uncomplicated they really are and can work on implementing them into your own programs.")
+
+
+while(i != 0):
+    i = input("""
+    Choose your lesson:
+    1. Introduction to Pointers
+    2. The Null Pointer
+    3. Pointers and Arrays
+    4. Pointers and Const
+    5. Pointers to Pointers
+    6. Cover it all!
+
+    0. Exit
+    """)
+
+    if i == 1:
+    elif i == 1:
+        Intro()
+        break
+    elif i == 2:
+    elif i == 3:
+    elif i == 4:
+    elif i == 5:
+    elif i == 6:
+    elif i == 0:
+    else:
+        print("""
+        Unexpected input. Choose your lesson:
+        1. Introduction to Pointers
+        2. The Null Pointer
+        3. Pointers and Arrays
+        4. Pointers and Const
+        5. Pointers to Pointers
+        6. Cover it all!
+
+        0. Exit
+        """)
+
+close = input("Press Enter to exit")
